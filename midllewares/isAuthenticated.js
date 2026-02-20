0b9a693cfcd67257dfb24add735b4a715cef446e
fjaquet <fjaquet@outlook.fr>
2026-02-20 21:52:13 +0100

    check token format and update error message

--- midllewares/isAuthenticated.js
+++ midllewares/isAuthenticated.js
@@ -1,13 +1,18 @@
 const User = require("../models/User");
 
 const isAuthenticated = async (req, res, next) => {
+  if (typeof req.headers.authorization !== "string") {
+    return res.status(401).json("Invalid token format");
+  }
+
   const token = req.headers.authorization.replace("Bearer ", "");
 
   const user = await User.findOne({ token: token });
+
   if (user) {
     req.user = user;
   } else {
-    return res.status(401).json("not authorized");
+    return res.status(401).json("Unauthorized");
   }
 
   next();
